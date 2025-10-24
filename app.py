138eb35c37171407da9553d868957d86f31aa621
khushraj-devloper <khushraj0071@gmail.com>
2025-10-24 17:50:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, render_template, send_from_directory,redirect,url_for,after_this_request
 import yt_dlp
 import os
@@ -7,7 +8,7 @@ import uuid
 app = Flask(__name__)
 DOWNLOAD_FOLDER = "downloads"
 
-def delayed_delete(file_path, delay=26):
+def delayed_delete(file_path, delay=30):
     def delete():
         time.sleep(delay)
         
@@ -17,36 +18,38 @@ def delayed_delete(file_path, delay=26):
 
 
 
-filename = f"{uuid.uuid4()}.mp3"
-
+filename = None
+unique_id = str(uuid.uuid4())
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         video_url = request.form["url"]
-        unique_id = str(uuid.uuid4())
-        filename = f"{unique_id}.mp3"
+      
+       
 
         ydl_opts = {
-            'format': 'bestaudio[ext=m4a]/bestaudio',
+            'format': 'best[height=240][ext=mp4]/best[ext=mp4]',
             'outtmpl': f'{DOWNLOAD_FOLDER}/{unique_id}.%(ext)s',
             'cookiefile': 'cookies.txt',
        
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            try:
-                info = ydl.extract_info(video_url, download=True)
-                filename=f"downloads/{info['title']}.{info['ext']}"
-            except:  
-                print("Erro")
+          
+            info = ydl.extract_info(video_url, download=True)
+            filename=f"downloads/{unique_id}.{info['ext']}"
+            os.rename(filename,f"downloads/{unique_id}.mp3")
+            filename=f"downloads/{unique_id}.mp3"
+            print("################",filename)
+             
             #filename = f"{info['title']}.mp3"  
             #filename = f"{uuid.uuid4()}.mp3"
 
       #  return redirect(url_for("download_file", filename=filename))
         
         
-        return redirect(url_for("thank_you", filename=filename))
+        return redirect(url_for("thank_you", filename=f"{unique_id}.mp3"))
 
 
     return render_template("index.html")
@@ -59,6 +62,7 @@ def thank_you(filename):
 @app.route("/download/<filename>")
 def download_file(filename):
 
+    print("before deleting file name is",filename)
     delayed_delete(f"downloads/{filename}")
     return send_from_directory(DOWNLOAD_FOLDER, filename, as_attachment=True) 
 
@@ -67,10 +71,8 @@ app.run(host="0.0.0.0", port=port)
 
 
 
-
-
-
 #it will download file and return to the server
+
 
 
 
