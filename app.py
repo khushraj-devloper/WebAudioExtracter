1810b3ec8fef5c04d8b57b89b79e430d70c8419d
khushraj-devloper <khushraj0071@gmail.com>
2025-10-24 18:04:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,8 @@ def home():
         ydl_opts = {
             'format': 'bestaudio[ext=m4a]/bestaudio[ext=webm]/bestaudio/best',
             'outtmpl': f'{DOWNLOAD_FOLDER}/{unique_id}.%(ext)s',
+            'postprocessors':[], 
+            
             'cookiefile': 'cookies.txt',
        
         }
@@ -72,6 +74,7 @@ app.run(host="0.0.0.0", port=port)
 
 
 #it will download file and return to the server
+
 
 
 
